6c4de33eb87c2313fbfc2a9633f96b2ededc6733
Jagapathi Pattabhi Ramayya Paluri <jagapathipaluri55@gmail.com>
2026-01-06 23:55:41 +0530

    commit from mainbranch for file localbranch2.py

--- localbranch1.py
+++ localbranch1.py
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
 print("localbranch1 file program ")
 print("second line is added in localbranch1")
 print("Third line is added in localbranch1")
+=======
+print("1")
+print("2")
+print("3")
+
+>>>>>>> Stashed changes
